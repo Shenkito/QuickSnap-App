d3ca8feb97ae30ccc0b5086f21cbad2f41fc9b27
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-05 15:37:51 +0200

    Add home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 
 import NavBar from './components/NavBar'
+import HomePage from './pages/Home'
 
 function App() {
 
     return (
         <>
             <NavBar/>
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+            </Routes>
         </>
     )
 }
