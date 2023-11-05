09fc9105fe3072874f42bb2b0af819016760b135
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-05 15:55:36 +0200

    Update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function HomePage(props) {
     return (
         <div>
             <div className="welcome-section">
-                <h1>Welcome to SnapThat. Your Social Media App.</h1>
+                <h1>Welcome to QuickSnap. Your Social Media App.</h1>
                 <p>Share your thoughts, connect with friends, and discover amazing content!</p>
                 <div className="button-container">
                     <Link to="/register" className="button">Get Started</Link>
