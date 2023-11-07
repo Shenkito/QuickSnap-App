b0a53fb3a9eb47e1a58930007f36ad31f437afea
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-07 21:03:04 +0200

    Some updates on the Home Page
    Extracted components in separate files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,40 +1,13 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import Welcome from "../components/Welcome";
+import LatestPosts from "../components/Latest";
 
 
-export default function HomePage(props) {
-
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts`)
-            .then((response) => response.json())
-            .then((result) => {
-                setPosts(result);
-            })
-
-    }, [])
+export default function HomePage() {
 
     return (
         <div>
-            <div className="welcome-section">
-                <h1>Welcome to QuickSnap. Your Social Media App.</h1>
-                <p>Share your thoughts, connect with friends, and discover amazing content!</p>
-                <div className="button-container">
-                    <Link to="/register" className="button">Get Started</Link>
-                </div>
-            </div>
-
-            <div className="container">
-                <h2>Latest Posts</h2>
-                {posts.map((post) => (
-                    <div className="post-card" key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.body}</p>
-                        <Link to={`/post/${post.id}`} className="post-button">See Post</Link>
-                    </div>
-                ))}
-            </div>
+            <Welcome />
+            <LatestPosts />
         </div>
 
     );
