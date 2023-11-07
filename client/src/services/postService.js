b6397721a63ce4bc8e89dce4d941de4fdb1c4373
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-07 21:00:40 +0200

    Added post services

---
+++ client/src/services/postService.js
@@ -0,0 +1,14 @@
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+export const getAll = async () => {
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        const data = Object.values(result);
+        
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
