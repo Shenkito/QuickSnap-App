0839d4d40a0f7d7f502571fdf27bfee57b6e2803
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-16 22:42:43 +0200

    Extract post card  as separete component
    Then use it in the main component

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+
+import Post from '../Posts/Post';
+
 import './Main.css'
 
 export default function Main() {
@@ -17,42 +20,7 @@ export default function Main() {
         <div className="element">
             <h1 className={showLatest ? 'latest show' : 'latest'}>Latest Posts</h1>
             <div className="row row-cols-1 row-cols-md-3 g-4">
-                <div className="col">
-                    <div className="card h-100">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6A8VojGxrMuOSUtEqrdhG_-HqGIEO9QMtdku8Fq-W50CS6muEWJPbJHWzzmCOs8gC0w&usqp=CAU" className="card-img-top" alt="Card 1" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card h-100">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJstSSmv0aB6wlalUPTlJzxkJyFOXdnhMhBsEc35EMXsOX1_uhCVwMgifEf3kZI3v5lwE&usqp=CAU" className="card-img-top" alt="Card 1" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card h-100">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVYn5zGLpW-TPd-XzRrMfLUy5YTjiZQQzS6sjUEBrkAkznfhQuMknU8jdJOboShXGzbM0&usqp=CAU" className="card-img-top" alt="Card 1" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-                </div>
+                <Post />
                 {/* Add more cards in similar structure */}
             </div>
         </div>
---
+++ client/src/components/Posts/Post.jsx
@@ -0,0 +1,18 @@
+import './Post.css'
+
+export default function Post() {
+    return (
+        <div className="col">
+            <div className="card h-100">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6A8VojGxrMuOSUtEqrdhG_-HqGIEO9QMtdku8Fq-W50CS6muEWJPbJHWzzmCOs8gC0w&usqp=CAU" className="card-img-top" alt="Card 1" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+        </div>
+    );
+}
