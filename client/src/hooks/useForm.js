dd5c9b53d5d62b727f1d65e834997e5de7434048
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-21 23:23:39 +0200

    Create custom hook useForm

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+// The custom hook for forms (for example see Login)
+
+export function useForm(submitHandler, initialValues) {
+    const [values, setValues] =  useState({initialValues});
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values)
+    }
+    
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
+}
