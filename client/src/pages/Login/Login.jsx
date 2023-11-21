1fe169ba7f3dc0b1df9cc9bea7600879a05835bf
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-21 23:24:33 +0200

    Changed the login form to controlled one

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,17 +1,29 @@
+import { useForm } from '../../hooks/useForm';
+
 import './Login.css'
 
 export default function Login() {
+    
+    
+    const { values, onChange, onSubmit } = useForm({ // Custom Hook -> controlled form (see bellow inputs)
+        //When mount those are the initial values
+        email: '',
+        password: '',
+    });
+
     return (
         <div className="login-container">
-            <form className="login-form">
+            <form className="login-form" onSubmit={onSubmit}>
                 <h2>Login</h2>
                 <div className="form-group">
-                    <label htmlFor="username">Username:</label>
+                    <label htmlFor="username">Email:</label>
                     <input
                         type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Enter your username"
+                        id="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        onChange={onChange}
+                        value={values.email}
                     />
                 </div>
                 <div className="form-group">
@@ -21,9 +33,11 @@ export default function Login() {
                         id="password"
                         name="password"
                         placeholder="Enter your password"
+                        onChange={onChange}
+                        value={values.password}
                     />
                 </div>
-                <button className="login-button" type="submit">LOGIN</button>
+                <button className="login-button" type="submit" onSubmit={onSubmit}>LOGIN</button>
                 <p>If you are not registered,  <a href="/register" className="register-link">click here</a> to register. </p>
             </form>
         </div>
