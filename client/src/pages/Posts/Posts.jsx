79e7fcb073c2f01b0ffd3d6890cf2cb1db4a478e
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-27 00:07:23 +0200

    Show all posts

--- client/src/pages/Posts/Posts.jsx
+++ client/src/pages/Posts/Posts.jsx
@@ -1,23 +1,56 @@
+import { useEffect, useState } from 'react';
+
+import * as postService from '../../services/postService';
+
+import PostCard from '../../components/main-post-card/PostCard';
+
 import './Posts.css'
 
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const postsObject = await postService.getAll();
+
+                // Check if the received data is an object
+                if (typeof postsObject === 'object' && postsObject !== null) {
+                    // Convert object keys into an array of posts
+                    const postsArray = Object.keys(postsObject).map((key) => ({
+                        _id: key, // Assuming the post ID is stored in the key
+                        ...postsObject[key], // Spread other post properties
+                    }));
+                    setPosts(postsArray);
+                } else {
+                    console.error('Received data is not in an object format:', postsObject);
+                    // You can handle this situation accordingly (e.g., set an empty array or show an error message)
+                    setPosts([]);
+                }
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
     return (
         <div className="posts-wrapper">
             <div className="post-cards">
-                <div key="{post.id}" className="card">
-                    <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6A8VojGxrMuOSUtEqrdhG_-HqGIEO9QMtdku8Fq-W50CS6muEWJPbJHWzzmCOs8gC0w&usqp=CAU" // Replace with the image source from your data
-                        alt=""
-                        className="card-img-top"
-                    />
-                    <div className="card-body">
-                        <h3 className="card-title">Title</h3>
-                        <p className="card-text">Description</p>
-                    </div>
-                    <div className="card-footer">
-                        <span className="text-body-secondary">Date</span>
-                    </div>
-                </div>
+                {Array.isArray(posts) && posts.length > 0 ? (
+                    posts.map((post) => (
+                        <PostCard
+                            key={post._id} // Assuming post._id is the unique identifier
+                            imageUrl={post.imageUrl}
+                            title={post.title}
+                            content={post.content}
+                            author={post.author}
+                        />
+                    ))
+                ) : (
+                    <p>No posts available</p>
+                )}
             </div>
             {/* Pagination component */}
             {/* Add pagination component here */}
