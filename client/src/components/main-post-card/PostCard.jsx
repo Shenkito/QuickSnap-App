b3244ecbd815c5aea3e648873767dce8f596ca9b
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-27 00:07:55 +0200

    Fix post card

--- client/src/components/main-post-card/PostCard.jsx
+++ client/src/components/main-post-card/PostCard.jsx
@@ -1,16 +1,16 @@
-import './PostCard.css'
+import './PostCard.css';
 
-export default function PostCard() {
+export default function PostCard({ imageUrl, title, content, author }) {
     return (
         <div className="col">
             <div className="card h-100">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6A8VojGxrMuOSUtEqrdhG_-HqGIEO9QMtdku8Fq-W50CS6muEWJPbJHWzzmCOs8gC0w&usqp=CAU" className="card-img-top" alt="Card 1" />
+                <img src={imageUrl} className="card-img-top" alt="Post" />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{content}</p>
                 </div>
                 <div className="card-footer">
-                    <small className="text-body-secondary">Last updated 3 mins ago</small>
+                    <small className="text-body-secondary">Author: {author}</small>
                 </div>
             </div>
         </div>
