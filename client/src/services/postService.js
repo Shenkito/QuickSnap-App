c0426d4c2011bc0198a1937319322da93bf205d7
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-27 00:05:04 +0200

    Add get all posts

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,5 +1,22 @@
 const baseUrl = 'http://localhost:3030/jsonstore'
 
+export const getAll = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/posts`);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
+        const posts = await response.json();
+        console.log(posts);
+        return posts;
+
+    } catch (error) {
+        console.log('Error fetching posts:', error);
+    }
+};
+
 export const create = async (postData) => {
     const response = await fetch(`${baseUrl}/posts`, {
         method: 'POST',
@@ -12,4 +29,4 @@ export const create = async (postData) => {
     const result = await response.json();
 
     return result;
-}
+};
