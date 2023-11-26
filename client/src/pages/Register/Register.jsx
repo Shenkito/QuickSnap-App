a337528ff3db305552953c1da93b401c61b8d8dd
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-26 15:56:14 +0200

    Fix the import of the service

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { register } from '../../services/userService';
+
+import * as userService from '../../services/userService';
 
 import './Register.css'
 
@@ -29,7 +30,7 @@ export default function Register() {
         e.preventDefault();
 
         try {
-            const user = await register(values);
+            const user = await userService.register(values);
             console.log(user);
         } catch (error) {
             console.log(error);
