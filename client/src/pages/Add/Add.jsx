871a5db602b19ca58f7b506a68b2538f94c19d5a
Ivelin Shenkov <ishenkito@gmail.com>
2023-11-26 14:29:47 +0200

    Fix add post form to be controlled

--- client/src/pages/Add/Add.jsx
+++ client/src/pages/Add/Add.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import './Add.css'
 
+const addFormKeys = {
+    Title: 'title',
+    Content: 'content',
+    Author: 'author'
+}
+
 export default function Add() {
+
+    const [formData, setFormData] = useState({
+        [addFormKeys.Title]: '',
+        [addFormKeys.Content]: '',
+        [addFormKeys.Author]: '',
+    });
+
+    const inputChangeHandler = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <div className="add-container">
-            <form className="post-form">
+            <form className="post-form" onSubmit={submitHandler}>
                 <h2>Add New Post</h2>
                 <div className="form-group">
                     <label htmlFor="post-title">Title:</label>
@@ -11,7 +38,9 @@ export default function Add() {
                         type="text"
                         id="post-title"
                         name="title"
+                        value={formData[addFormKeys.Title]}
                         placeholder="Enter the post title"
+                        onChange={inputChangeHandler}
                     />
                 </div>
                 <div className="form-group">
@@ -19,7 +48,9 @@ export default function Add() {
                     <textarea
                         id="post-content"
                         name="content"
+                        value={formData[addFormKeys.Content]}
                         placeholder="Enter the post content"
+                        onChange={inputChangeHandler}
                     />
                 </div>
                 <div className="form-group">
@@ -28,7 +59,9 @@ export default function Add() {
                         type="text"
                         id="post-author"
                         name="author"
+                        value={formData[addFormKeys.Author]}
                         placeholder="Enter your name"
+                        onChange={inputChangeHandler}
                     />
                 </div>
                 <button className="post-button" type="submit">SUBMIT</button>
