f8c5bd63fc2ce4e22d4648d9cd060263b950f437
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-01 22:45:55 +0200

    Fix the post services
    Add X-Auth token when create post

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,4 +1,6 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+const baseUrl = 'http://localhost:3030/data'
+
+import { getStoredUser } from './userService';
 
 export const getAll = async () => {
     try {
@@ -18,15 +20,28 @@ export const getAll = async () => {
 };
 
 export const create = async (postData) => {
+    const storedUser = getStoredUser(); // Retrieve user data containing the access token
+
+    if (!storedUser || !storedUser.accessToken) {
+        throw new Error('Access token not found');
+    }
+
+    const { accessToken } = storedUser;
+
     const response = await fetch(`${baseUrl}/posts`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-Authorization': accessToken, // Include the access token in the header
         },
-        body: JSON.stringify(postData)
-    })
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to create post');
+    }
 
     const result = await response.json();
-
     return result;
+
 };
