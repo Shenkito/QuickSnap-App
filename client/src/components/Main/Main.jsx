4f9d8b8e3a2a84143f059d14265cb0fa018751df
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-01 22:53:40 +0200

    Set posts in the state
    Also sorted latest posts to get the last 3 by date (need better solution)

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,28 +1,70 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import PostCard from '../post-card/PostCard';
 
-import PostCard from '../main-post-card/PostCard';
-
-import './Main.css'
-
+import * as postService from '../../services/postService';
+import './Main.css';
 
 export default function Main() {
 
     const [showLatest, setShowLatest] = useState(false);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowLatest(true);
-        }, 1000);
+        const fetchAndSetPosts = async () => {
+            try {
+                // Assuming postService.getAll() fetches posts
+                const postsObject = await postService.getAll();
 
-        return () => clearTimeout;
+                // Check if the received data is an object
+                if (typeof postsObject === 'object' && postsObject !== null) {
+                    // Convert object keys into an array of posts
+                    const postsArray = Object.keys(postsObject).map((key) => ({
+                        _id: key, // Assuming the post ID is stored in the key
+                        ...postsObject[key], // Spread other post properties
+                    }));
+
+                    // Sort posts by createdAt in descending order to get the latest first
+                    const sortedPosts = postsArray.sort((a, b) => new Date(b._createdOn) - new Date(a._createdOn));
+
+                    // Take only the first three posts (the latest)
+                    const latestPosts = sortedPosts.slice(0, 3);
+
+                    setPosts(latestPosts);
+                } else {
+                    console.error('Received data is not in an object format:', postsObject);
+                    // You can handle this situation accordingly (e.g., set an empty array or show an error message)
+                    setPosts([]);
+                }
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchAndSetPosts().then(() => {
+            const timer = setTimeout(() => {
+                setShowLatest(true);
+            }, 1000);
+
+            return () => clearTimeout(timer); // Clear the timeout
+        });
     }, []);
 
     return (
-        <section className="element">
-            <h1 className={showLatest ? 'latest show' : 'latest'}>Latest Posts</h1>
+        <section className={`element ${showLatest ? 'show' : ''}`}>
+            <h1 className="latest">Latest Posts</h1>
             <div className="row row-cols-1 row-cols-md-3 g-4">
-                <PostCard />
-                {/* Add more cards in similar structure */}
+                {showLatest &&
+                    posts.map((post) => (
+                        <PostCard
+                            key={post._id} {...post}
+                        // imageUrl={post.imageUrl}
+                        // title={post.title}
+                        // content={post.content}
+                        // _ownerId={post._ownerId}
+                        // _id={post._id}
+                        // description={post.description}
+                        />
+                    ))}
             </div>
         </section>
     );
