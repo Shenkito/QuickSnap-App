837f8bc6e219fe857dc926e86a32e90356041bb8
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-01 22:44:57 +0200

    Fix the user services

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,53 +1,77 @@
 const baseUrl = 'http://localhost:3030/users'
 
-export const login = async (email, password) => {
-    
+export const serializeUser = (user) => {
+    return JSON.stringify(user);
+};
+
+const deserializeUser = (serializedUser) => {
+    return JSON.parse(serializedUser);
+};
+
+export const login = async ({ email, password }) => {
     try {
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            headers['X-Authorization'] = token;
+        }
+
         const response = await fetch(`${baseUrl}/login`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(email, password), 
+            headers: headers,
+            body: JSON.stringify({ email, password }), // Send email and password as an object
         });
 
-
-        if(!response.ok) {
-            throw new Error('Login failed')
+        if (!response.ok) {
+            throw new Error('Login failed');
         }
 
         const result = await response.json();
-        return result
+        localStorage.setItem('user', serializeUser(result)); // Serialize user data
+        return result;
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        throw err;
     }
-}
+};
 
-export const register = async (email, password) => {
+export const register = async ({ email, password }) => {
     try {
+        const headers = {
+            'Content-Type': 'application/json',
+        };
 
-        // const requestBody = {
-        //     email: email,
-        //     password: password,
-        //     // You can add other properties here if required
-        // };
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            headers['X-Authorization'] = token;
+        }
 
         const response = await fetch(`${baseUrl}/register`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(email, password), 
+            headers: headers,
+            body: JSON.stringify({ email, password }), // Send email and password as an object
         });
 
-        if(!response.ok) {
-            throw new Error('Registration failed')
+        if (!response.ok) {
+            throw new Error('Registration failed');
         }
-        
-        const result = await response.json();
 
-        return result
+        const result = await response.json();
+        localStorage.setItem('user', serializeUser(result)); // Serialize user data
+        return result;
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        throw err;
     }
-}
+};
+
+export const getStoredUser = () => {
+    const serializedUser = localStorage.getItem('user');
+    if (serializedUser) {
+        return deserializeUser(serializedUser); // Deserialize user data
+    }
+    return null;
+};
