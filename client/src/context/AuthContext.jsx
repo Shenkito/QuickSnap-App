c3f3a771b4a43b4b59396aa42a5303620b83f294
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-04 19:55:12 +0200

    Add auth context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from 'react';
+import * as userService from '../services/userService'
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(userService.getStoredUser()); // Retrieve stored user from local storage
+
+    const login = async ({ email, password }) => {
+        try {
+            const loggedInUser = await userService.login({ email, password });
+            setUser(loggedInUser);
+            return loggedInUser;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    };
+
+    const register = async ({ email, password }) => {
+        try {
+            const registeredUser = await userService.register({ email, password });
+            setUser(registeredUser);
+            return registeredUser;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    };
+
+    const logout = () => {
+        // Perform logout actions, e.g., clear local storage, reset state, etc.
+        localStorage.removeItem('user');
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, register, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
