9405e9e36c905c977e16080fc645ecd6ebf6cdbc
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-05 20:34:00 +0200

    Add profile

---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import { useAuth } from '../../context/AuthContext';
+
+import './Profile.css'; // Import the CSS file for styling
+
+const Profile = () => {
+
+    const { user } = useAuth();
+
+    return (
+        <div className="profile-container">
+            <div className="profile-card">
+                <div className="profile-header">
+                    <img className="profile-image" src={user.profileImage} alt="Profile" />
+                    <h2>{user.username}</h2>
+                    <p className="email">Email: {user.email}</p>
+                </div>
+                <div className="profile-details">
+                    <h3>Bio:</h3>
+                    <p>{user.bio}</p>
+                    {/* Add more profile details here */}
+                </div>
+                {/* Add more sections for additional user details */}
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
