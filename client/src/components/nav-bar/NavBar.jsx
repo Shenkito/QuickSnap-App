3ce2b397f51c92a0e7bef90baacd7d33c3dbe89a
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-05 20:29:03 +0200

    Show user on nav bar if there is
    Showing different navbar if logged or guest.
    Also add logout

--- client/src/components/nav-bar/NavBar.jsx
+++ client/src/components/nav-bar/NavBar.jsx
@@ -1,38 +1,55 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
-import './NavBar.css'
+import './NavBar.css';
 
 export default function NavBar() {
-    const { logout } = useAuth()
+
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
 
     const logoutHandler = () => {
         logout();
-    }
+        navigate('/');
+    };
 
     return (
         <ul className="nav nav-underline">
+            {user ? (
+                <li className="nav-item user-info">Logged as {user.username}</li>
+            ) : null}
             <li className="nav-item">
                 <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/posts">All Posts</Link>
             </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/posts/add">Add Post</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/register">Register</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/profile">Profile</Link>
-            </li>
-            <li className="nav-item" onClick={logoutHandler}>
-                <Link className="nav-link" to="/">Logout</Link>
-            </li>
+
+            {user ? ( // Check if there is a logged-in user
+                <>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/posts/add">Add Post</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/profile">Profile</Link>
+                    </li>
+                    <li className="nav-item" onClick={logoutHandler}>
+                        <Link className="nav-link" to="#">
+                            Logout
+                        </Link>
+                    </li>
+                </>
+            ) : (
+                <>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Register</Link>
+                    </li>
+                </>
+            )}
         </ul>
     );
 }
