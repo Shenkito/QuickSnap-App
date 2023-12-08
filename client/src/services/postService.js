4b0d70345f2356452eaabf526752c5046b6bbb20
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-08 21:57:48 +0200

    Add update (edit) to post service

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -28,7 +28,6 @@ export const getOne = async (postId) => {
         }
 
         const post = await response.json();
-        console.log(post);
         return post;
     } catch (error) {
         console.log('Error fetching post:', error);
@@ -50,6 +49,35 @@ export const create = async (postData) => {
         headers: {
             'Content-Type': 'application/json',
             'X-Authorization': accessToken, // Include the access token in the header
+
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to create post');
+    }
+
+    const result = await response.json();
+    return result;
+
+};
+
+export const update = async (postData, postId) => {
+    const storedUser = getStoredUser(); // Retrieve user data containing the access token
+
+    if (!storedUser || !storedUser.accessToken) {
+        throw new Error('Access token not found');
+    }
+
+    const { accessToken } = storedUser;
+
+    const response = await fetch(`${baseUrl}/posts/${postId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': accessToken, // Include the access token in the header
+
         },
         body: JSON.stringify(postData),
     });
