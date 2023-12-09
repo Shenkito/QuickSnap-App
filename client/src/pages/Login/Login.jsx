e0867adb4de8c110622cf5e0176a2cd394c57fc9
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-09 23:48:01 +0200

    Add validation on login
    And link if not registered

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,15 @@
+// import { useForm } from '../../hooks/useForm';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../../context/AuthContext';
+// import * as userService from '../../services/userService';
+
+import { Link } from 'react-router-dom';
 
 import './Login.css'
 
-
+//Upgrade
 const LoginFormKeys = {
     Email: 'email',
     Password: 'password'
@@ -32,17 +36,44 @@ export default function Login() {
     const submitHandler = async (e) => {
         e.preventDefault();
 
+        const { email, password } = values;
+
+        // Check if email or password fields are empty
+        if (!email || !password) {
+            alert('Please fill in both email and password fields.');
+            return;
+        }
+
         try {
             const user = await login({
                 email: values[LoginFormKeys.Email],
                 password: values[LoginFormKeys.Password],
             });
 
+            // Serialize and store the user data after successful login
+            // localStorage.setItem('user', userService.serializeUser(user));
+
             navigate('/');
         } catch (error) {
-            console.log(error);
+            alert(error)
         }
     };
+
+    //With useForm
+    // const loginSubmitHandler = async (data) => {
+    //     try{
+    //         const user = await login(data);
+    //         console.log(user);
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // }
+
+    // const { values, onChange, onSubmit } = useForm(loginSubmitHandler,{ // Custom Hook -> controlled form (see bellow inputs)
+    //     //When mount those are the initial values
+    //     [LoginFormKeys.Email]: '',
+    //     [LoginFormKeys.Password]: '',
+    // });
 
     return (
         <div className="login-container">
@@ -71,7 +102,8 @@ export default function Login() {
                     />
                 </div>
                 <button className="login-button" type="submit">LOGIN</button>
-                <p>If you are not registered,  <a href="/register" className="register-link">click here</a> to register. </p>
+                <p>
+                    If you are not registered, <Link to="/register" className="register-link">click here</Link> to register.</p>
             </form>
         </div>
     );
