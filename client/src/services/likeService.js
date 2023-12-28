997afca3cecb9f343aeb220303ec51f4f3a75bae
Ivelin Shenkov <ishenkito@gmail.com>
2023-12-28 20:46:40 +0200

    Add unlike and check if user liked a post

--- client/src/services/likeService.js
+++ client/src/services/likeService.js
@@ -30,6 +30,56 @@ export const like = async (likeData) => {
 
 };
 
+export const unlike = async (likeId) => {
+    const storedUser = getStoredUser();
+
+    if (!storedUser || !storedUser.accessToken) {
+        throw new Error('Access token not found');
+    }
+
+    const { accessToken } = storedUser;
+
+    const response = await fetch(`${baseUrl}/likes/${likeId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': accessToken,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to unlike the post');
+    }
+
+    const result = await response.json();
+    return result;
+};
+
+export const checkUserLikedPost = async (postId, userId) => {
+    try {
+        const storedUser = getStoredUser();
+
+        if (!storedUser || !storedUser.accessToken) {
+            throw new Error('Access token not found');
+        }
+
+        const { accessToken } = storedUser;
+
+        const response = await fetch(
+            `${baseUrl}/likes?where=PostId%3D%22${postId}%22&where=Author%3D%22${userId}%22`
+        );
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch user liked post');
+        }
+
+        const userLikedPost = await response.json();
+        return userLikedPost.length > 0; // Return true if the user has liked the post, false otherwise
+    } catch (error) {
+        throw new Error('Error checking if the user liked the post');
+    }
+};
+
 export const getByPostId = async (postId) => {
     try {
         const response = await fetch(`${baseUrl}/likes?where=PostId%3D%22${postId}%22`);
